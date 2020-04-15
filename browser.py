1431032af612d27c9139f9a066b755f9232666b5
Dmitriy Struzhkin <struzhkin.dmitriy@gmail.comgit config --global user.email struzhkin.dmitriy@gmail.comgit config --global user.email struzhkin.dmitriy@gmail.com>
2020-04-15 19:41:44 +0300

    Stage without request to real url

--- browser.py
+++ browser.py
@@ -1,4 +1,6 @@
-nytimes_com = '''
+import os
+
+nytimes = '''
 This New Liquid Is Magnetic, and Mesmerizing
  
 Scientists have created “soft” magnets that can flow 
@@ -13,7 +15,7 @@ Jessica Wade has added nearly 700 Wikipedia biographies for
  years.
  
 '''
-bloomberg_com = '''
+bloomberg = '''
 The Space Race: From Apollo 11 to Elon Musk
  
 It's 50 years since the world was gripped by historic images
@@ -32,23 +34,65 @@ Twitter and Square Chief Executive Officer Jack Dorsey
  Tuesday, a signal of the strong ties between the Silicon Valley giants.
 '''
 
+def menu():
+	user_url = input()
+	history_list.append(user_url)
+	if user_url == 'back':
+		del history_list[-2:]
+		user_url = history_list[-1]
 
-class Browser(object):
-    url_list = {}
+	if "dir" in user_url:
+		create_dir(user_url)
+	elif "." in user_url and user_url in text_list:
+		pages_list.append(page_url(user_url))
+		writer(user_url)
+		print(text_list[user_url])
+	elif user_url == "exit":
+		exit(0)
+	elif user_url in pages_list:
+		open_page(user_url)
+	else:
+		print("error")
 
-    def __init__(self, name, inc):
-        self.name = name
-        self.inc = inc
-        Browser.url_list[name] = inc
-
-    def search(search_url):
-        for key, i in Browser.url_list.items():
-            if key == search_url:
-                return i
+	return menu()
 
 
-nytimes_com_url = Browser('nytimes.com', nytimes_com)
-bloomberg_com_url = Browser('bloomberg.com', bloomberg_com)
 
-print(Browser.search(input()))
+def create_dir(user_url):
+    try:
+        os.mkdir(user_url)
+        # print("Directory " , user_url,  " Created ")
+        global dir_name
+        dir_name = user_url
+    except FileExistsError:
+        print("error")
+        return menu()
 
+
+def writer(user_url):
+	if not os.path.exists(dir_name):
+		os.mkdir(dir_name)
+		with open(f'{dir_name}/{page_url(user_url)}.txt', 'w') as f:
+			f.write(text_list[user_url])
+	else:
+		with open(f'{dir_name}/{page_url(user_url)}.txt', 'w') as f:
+			f.write(text_list[user_url])
+
+def page_url(user_url):
+    # Return name url
+    return user_url.rsplit(".").pop(0)
+
+
+def open_page(user_url):
+	try:
+	    with open(f'{dir_name}/{user_url}.txt') as f:
+	        for line in f:
+	        	print(line.strip())
+	except FileNotFoundError:
+		print('error')
+
+history_list = []
+pages_list = []
+dir_name = "tb_tabs"
+text_list = {'nytimes.com': nytimes, 'bloomberg.com': bloomberg}
+menu()
